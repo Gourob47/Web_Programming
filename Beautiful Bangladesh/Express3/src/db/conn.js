2f4b23d2667b54e2fb4d9a28bfeffe7cca4b1167
Gourob47 <85252503+Gourob47@users.noreply.github.com>
2023-02-23 22:14:11 +0600

    Edited

--- Beautiful Bangladesh/Express3/src/db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-
-mongoose.connect('mongodb://localhost:27017/NewData', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(()=>{
-  console.log(`Connection Successsful`);
-}).catch(()=>{
-    console.log(`Connection unSuccesssful`);
-})
